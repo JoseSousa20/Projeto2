b439c2dab43b63cca3df9e5bed3cee218759b1d7
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-29 10:20:41 +0000

    Update .editorconfig, .env.example, and 91 more files...

---
+++ app/Http/Controllers/LivrosController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Livro;
+
+class LivrosController extends Controller
+{
+    //
+    public function index(){
+    //$livro = Livro::all()->sortbydesc('idl');
+    $livro = Livro::paginate(4);
+    return view ('livros.index', [
+        'livros'=>$livro
+    ]);
+    }
+
+}
