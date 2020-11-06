c63b08eae92f4833f09add33a05612a8f637b3f2
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-06 17:27:23 +0000

    Update enviado.blade.php, index.blade.php, and 2 more files...

--- resources/views/livros/index.blade.php
+++ resources/views/livros/index.blade.php
@@ -14,4 +14,11 @@
 @endforeach
 </ul>
 {{$livros->render()}}
+
+
+<h5>Pesquisar Livros</h5>
+<form method="POST" action="enviado">
+    Pesquisar: <input type="text" name="pesquisar">
+    <input type="submit" value="Enviar">
+</form>
 @endsection
